02f3348de4a98b73a879fc407b09be72242b2134
Chloe Zermatten <chloezermatten@gmail.com>
2023-11-23 10:34:43 +0000

    not sure what happened here - all up to date now

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,28 @@
-import { useState } from 'react'
-import { createContext } from 'react';
-import CreatePost from './components/CreatePost.jsx';
-import Header from './components/Header.jsx'
-import Posts from './components/Posts.jsx'
-import './App.css'
+import { useState } from "react";
+import { createContext } from "react";
+import CreatePost from "./components/CreatePost.jsx";
+import Header from "./components/Header.jsx";
+import Posts from "./components/Posts.jsx";
+import "./App.css";
 
-const APP_NAME = 'Next-gen Social Media'
-const PostsContext = createContext()
+const APP_NAME = "Next-gen Social Media";
+const PostsContext = createContext();
 
 function App() {
-    const [posts, setPosts] = useState([
-        { title: 'Hello, world!', content: 'React context is great :)' },
-        { title: 'But...', content: 'It\'s a little confusing at first!' },
-    ])
+  const [posts, setPosts] = useState([
+    { title: "Hello, world!", content: "React context is great :)" },
+    { title: "But...", content: "It's a little confusing at first!" },
+  ]);
 
-    return (
-        <>
-        <PostsContext.Provider value={{posts, setPosts, APP_NAME}}>
-            <Header />
-            <CreatePost />
-            <Posts />
-        </PostsContext.Provider>
-        </>
-    )
+  return (
+    <>
+      <PostsContext.Provider value={{ posts, setPosts, APP_NAME }}>
+        <Header />
+        <CreatePost />
+        <Posts />
+      </PostsContext.Provider>
+    </>
+  );
 }
 
-export { App, PostsContext }
+export { App, PostsContext };
